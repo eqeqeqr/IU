bd23b14b09bbe2b04101faf697658b2476eea805
1500292505 <1500292505@qq.com>
2023-12-09 16:51:24 +0800

    iu

---
+++ src/main/java/com/example/iu/IuApplication.java
@@ -0,0 +1,13 @@
+package com.example.iu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IuApplication.class, args);
+    }
+
+}
